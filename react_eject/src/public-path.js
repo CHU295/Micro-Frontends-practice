9c08bf2a4c18827e85478a04799ecfaa90951dce
chuxk <chuxiaokang@corp.netease.com>
2021-09-30 17:09:45 +0800

    添加eslint项

--- react_eject/src/public-path.js
+++ react_eject/src/public-path.js
@@ -1,3 +1,5 @@
 if (window.__POWERED_BY_QIANKUN__) {
+  /* global __webpack_public_path__:writable */
+  /* exported __webpack_public_path__ */
   __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__;
 }
